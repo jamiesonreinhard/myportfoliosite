11f61f904541f48c22ffb0eaabacd47cbcffe85c
jamiesonreinhard <59634117+jamiesonreinhard@users.noreply.github.com>
2020-05-28 17:48:24 -0600

    second commit

---
+++ jmr-portfolio/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Hero extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h1 className="header-primary"></h1>
+            </div>
+        )
+    }
+}
