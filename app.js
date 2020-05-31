9bb4b371bc410850c391f8bef841c3f745e12f2c
jamiesonreinhard <59634117+jamiesonreinhard@users.noreply.github.com>
2020-05-31 16:13:00 -0600

    heroku fix try again

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static('client/build'));
+  app.use(express.static('jmr-portfolio/build'));
 
   // Express serve up index.html file if it doesn't recognize route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'jmr-portfolio', 'build', 'index.html'));
   });
 }
 
