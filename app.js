0c9b1d7d8ac0a1fb5ecf528dcd2e79344c3a7506
jamiesonreinhard <59634117+jamiesonreinhard@users.noreply.github.com>
2020-05-31 16:22:48 -0600

    changed public to build

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static('jmr-portfolio/public'));
+  app.use(express.static('jmr-portfolio/build'));
 
   // Express serve up index.html file if it doesn't recognize route
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'jmr-portfolio', 'public', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'jmr-portfolio', 'build', 'index.html'));
   });
 }
 
