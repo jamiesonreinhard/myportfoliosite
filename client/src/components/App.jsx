49753dbcc75ca5f71fabde3eb00440a63a189eb4
jamiesonreinhard <59634117+jamiesonreinhard@users.noreply.github.com>
2020-06-01 09:15:29 -0600

    updated App.jsx to fetch express server

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 
 import Hero from "./Hero";
 import Navigation from "./Navigation";
@@ -8,18 +8,39 @@ import Projects from "./Projects";
 import Contact from "./Contact";
 import Footer from "./Footer";
 
-function App() {
-  return (
-    <div className="appdiv">
-      <Navigation />
-      <Hero />
-      <About />
-      <Resume />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
-  );
-}
+export default class App extends Component {
+  state = {
+    data: null
+  };
 
-export default App;
+  componentDidMount() {
+      // Call our fetch function below once the component mounts
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }
+    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+    return body;
+  };
+
+  render(){
+    return (
+      <div className="appdiv">
+        <Navigation />
+        <Hero />
+        <About />
+        <Resume />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
+    );
+  }
+}
