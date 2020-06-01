1a055fd007c4832c2f64d18d04c3112f4a628e2e
jamiesonreinhard <59634117+jamiesonreinhard@users.noreply.github.com>
2020-06-01 09:40:16 -0600

    changed app.js path to index.html

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
 const app = express();
-const publicPath = path.join(__dirname, 'client', 'public');
+const publicPath = path.join(__dirname, '/client/build');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(publicPath));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
-});
+        app.get('*', function(req, res) {
+          res.sendFile('index.html', { root: __dirname }, function(err) {
+            if (err) {
+              res.status(500).send(err);
+            }
+          });
+        });
 
 
 
